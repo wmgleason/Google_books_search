49839ca061ff90a0db58aa32e5f7fbec2c9caedb
Wanda Gleason <oglissane@gmail.com>
2021-05-16 17:31:08 -0400

    Function to delte a book fr the database.

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -31,4 +31,11 @@ module.exports = {
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   },
+  // used to delete a book from the db
+  remove: (req, res) => {
+    db.Book.findById({ _id: req.params.id })
+      .then((dbModel) => dbModel.remove())
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
 };
