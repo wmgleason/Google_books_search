1ae7136897c18a9840e5908784354fc8b84cdcc0
Wanda Gleason <oglissane@gmail.com>
2021-05-16 17:14:15 -0400

    Save button.

---
+++ client/src/components/SaveBtn/index.js
@@ -0,0 +1,49 @@
+// need useState to maintain state in functional component
+import React, { useState } from "react";
+// used for saving book modal
+import { Modal, Button } from "react-bootstrap";
+
+// Using React Hooks maintains state
+// Applying the className, children and onClick props to the save button element
+function SaveBtn({ type = "default", className, children, onClick }) {
+  const [show, setShow] = useState(false);
+
+  // handles closing of modal window
+  const handleClose = () => setShow(false);
+  // handles showing of modal
+  const handleShow = () => setShow(true);
+
+  // saves book to the database and brings up the modal window
+  conts click = () => {
+    handleShow();
+    onClick();
+  };
+
+  return (
+    <>
+      <button
+        onClick={click}
+        className={["btn btn-lg", `btn-${type}`, className].join(" ")}
+      >
+        {children}
+      </button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Success - your book has been saved!</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>View your saved books in the Saved tab.</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            OK
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
+export default SaveBtn;
