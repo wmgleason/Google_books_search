ce6a8256dfec602e29bf11e4bc38efdc5eb13f10
Wanda Gleason <oglissane@gmail.com>
2021-05-16 16:57:11 -0400

    Commented.

--- client/src/components/Thumbnail/index.js
+++ client/src/components/Thumbnail/index.js
@@ -1,9 +1,6 @@
 import React from "react";
 import "./style.css";
-
-// The Thumbnail component renders a div that uses some CSS to render a background image
-// It will always keep square proportions at any size without the image warping
-// The "role" and "aria label" are there to identify the element's purpose as an image for accessibility purposes
+// thumbnail for book image
 function Thumbnail({ src }) {
   return (
     <div
