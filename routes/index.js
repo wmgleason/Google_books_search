63888786b78df5e92a592c042ea6e60bf0ecbc25
Wanda Gleason <oglissane@gmail.com>
2021-05-16 19:11:49 -0400

    Routes for findall and findbyid.

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+
+const booksController = require("../controllers/booksController");
+
+router
+  .route("/books")
+  .get(booksController.findAll)
+  // .get(booksController.searchApi)
+  .post(booksController.save);
+
+router
+  .route("/books/:id")
+  .get(booksController.findById)
+  .put(booksController.update)
+  .delete(booksController.remove);
+
+module.exports = router;
