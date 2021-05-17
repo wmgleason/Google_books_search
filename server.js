207ebc7be97643462f02b0c5035beffd82fbce60
Wanda Gleason <oglissane@gmail.com>
2021-05-16 22:56:14 -0400

    Had forgotten dotenv.

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const apiRoutes = require("./routes/index");
-const path = require("path");
+require("dotenv").config();
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// .env to hide sensitive info
+// .env
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
