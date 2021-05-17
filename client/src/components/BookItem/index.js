74abe25ea79422128a49cad05f672d67a233b4af
Wanda Gleason <oglissane@gmail.com>
2021-05-16 23:29:42 -0400

    Image container and submit button.

---
+++ client/src/components/BookItem/index.js
@@ -0,0 +1,58 @@
+import React from "react";
+
+import "./style.css";
+
+const BookItem = (props) => {
+  const { book, saveBook, deleteBook, saved } = props;
+
+  return (
+    <div className="book">
+      <div className="book-box">
+        <h4>{book.title}</h4>
+        {book.description && <p>{book.description}</p>}
+
+        {book.authors && (
+          <p>
+            By: <strong>{book.authors.join(", ")} </strong>
+          </p>
+        )}
+
+        <div className="img-container">
+          {saved && book.img && <img src={book.img} alt={""} />}
+          {!saved && book.imageLinks && (
+            <img src={book.imageLinks.smallThumbnail} alt={""} />
+          )}
+        </div>
+        <div className="actions">
+          <a
+            href={book.infoLink}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="theme-link"
+          >
+            View
+          </a>
+          {saved ? (
+            <button
+              type="submit"
+              className="theme-btn"
+              onClick={() => deleteBook(book._id)}
+            >
+              Delete
+            </button>
+          ) : (
+            <button
+              type="submit"
+              className="theme-btn"
+              onClick={() => saveBook(book)}
+            >
+              Save
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BookItem;
