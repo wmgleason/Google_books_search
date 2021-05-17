47d9ff71e556dacd46f64322ea61980e56b45f3f
Wanda Gleason <oglissane@gmail.com>
2021-05-17 01:08:36 -0400

    Added app.use for client/public.

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use("/public", express.static(path.join(__dirname, "client/public")));
 // .env
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
