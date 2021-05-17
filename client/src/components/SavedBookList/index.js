1dbf6a5d2644ea0861a15a38e3da5a9e66b1a34a
Wanda Gleason <oglissane@gmail.com>
2021-05-16 23:36:26 -0400

    Needed saved book list.

---
+++ client/src/components/SavedBookList/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import BookItem from "../BookItem";
+
+const SavedBookList = (props) => {
+  const { books, deleteBook } = props;
+
+  return (
+    <div className="books">
+      <h3>Books</h3>
+      <hr />
+      {books.map((book, index) => (
+        <BookItem
+          key={index}
+          book={book}
+          deleteBook={(id) => deleteBook(id, index)}
+          saved={true}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default SavedBookList;
