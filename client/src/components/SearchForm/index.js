ffed256803064aa4814ed7bf3f1961bd284df116
Wanda Gleason <oglissane@gmail.com>
2021-05-16 23:41:49 -0400

    Added search form.

---
+++ client/src/components/SearchForm/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+import "./style.css";
+
+const SearchForm = (props) => {
+  const { handleChange, search, searchValue } = props;
+
+  return (
+    <div className="search">
+      <h3>Book Search</h3>
+      <div className="input-search">
+        <input
+          type="text"
+          value={searchValue}
+          className="theme-input"
+          onChange={handleChange}
+          placeholder="Please Search Book Title"
+          required
+        />
+        <button type="submit" className="theme-btn" onClick={search}>
+          Search
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SearchForm;
